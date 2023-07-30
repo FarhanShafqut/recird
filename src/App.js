ac2797e59c2b004f212cf8b5a9586e600d88e82b
M.Farhan Shafqut <125457388+FarhanShafqut@users.noreply.github.com>
2023-07-30 21:24:24 +0500

    TO DO

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  const handleAddTask = () => {
+    if (taskInput.trim() === '') return;
+    setTasks([...tasks, taskInput]);
+    setTaskInput('');
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div className="task-container">
+        <input
+          type="text"
+          value={taskInput}
+          onChange={(e) => setTaskInput(e.target.value)}
+          placeholder="Enter your task..."
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
